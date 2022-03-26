d68b25f8cfe69aff3a0b55195b80c1714680f604
Muzhik75 <muzhik75@gmail.com>
2022-03-26 16:50:37 +0300

    begin

---
+++ Lession_005/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lession_005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Конец программы ");
+        }
+    }
+}
